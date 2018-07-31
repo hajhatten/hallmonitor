ba2c3a25c27ffc8d72a933588d86ac8e40dabfb5
Rikard Gynnerstedt <rikard.gynnerstedt@gmail.com>
2018-07-31 21:44:05 +0200

    added http server and static website to display data

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 const (
@@ -24,7 +26,103 @@ var (
 	apikeyStopSign = ""
 )
 
-type apiresponse struct {
+func main() {
+
+	flag.BoolVar(&debug, "d", false, "print debug messages")
+	flag.Parse()
+
+	apikeyStopSign = os.Getenv("RESROBOTAPIKEY")
+	if apikeyStopSign == "" {
+		log.Fatal("no apikey present, use env var RESROBOTAPIKEY to set it")
+	}
+
+	if debug {
+		// Print result
+
+		apiRespBody, err := readStopSignAPI()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var slResponse slAPIResponse
+		if err := json.Unmarshal(*apiRespBody, &slResponse); err != nil {
+			log.Fatal(err)
+		}
+
+		result := sortAPIResponse(slResponse)
+
+		fmt.Println("Bussar in mot stan: \n ")
+		fmt.Println("Linje:\tDestination:\tAnkomst:\tOm:")
+		printArrivals(result.ArrivalsInToCity)
+
+		fmt.Println("\nBussar ut från stan: \n ")
+		fmt.Println("Linje:\tDestination:\t\tAnkomst:\tOm:")
+		printArrivals(result.ArrivalsOutOfCity)
+	} else {
+		router := mux.NewRouter()
+		router.HandleFunc("/halltider", halltiderAPI)
+		router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+		srv := &http.Server{
+			Handler:      router,
+			Addr:         ":3001",
+			WriteTimeout: 15 * time.Second,
+			ReadTimeout:  15 * time.Second,
+		}
+		log.Fatal(srv.ListenAndServe())
+	}
+
+}
+
+func halltiderAPI(w http.ResponseWriter, r *http.Request) {
+
+	apiRespBody, err := readStopSignAPI()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var slResponse slAPIResponse
+	if err := json.Unmarshal(*apiRespBody, &slResponse); err != nil {
+		log.Println(err)
+		return
+	}
+
+	response, err := json.Marshal(sortAPIResponse(slResponse))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	_, err = w.Write(response)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func printArrivals(arrivals []arrival) {
+	for _, arrival := range arrivals {
+		deptime, err := time.Parse("2006-01-02T15:04:05", fmt.Sprintf("%sT%s", arrival.Date, arrival.Time))
+		if err != nil {
+			log.Fatal(err)
+		}
+		delta := deptime.Sub(time.Now().Add(time.Hour * 2)).Truncate(time.Minute)
+
+		switch arrival.Origin {
+		case "Spånga station (Stockholm kn)":
+			fmt.Printf("%s \tAlvik \t\t%s\t%.0fm \n", arrival.TransportNumber, arrival.Time, math.Ceil(delta.Minutes()))
+		case "Blackebergs gård (Stockholm kn)":
+			fmt.Printf("%s \tSolna centrum \t%s \t%.0fm \n", arrival.TransportNumber, arrival.Time, math.Ceil(delta.Minutes()))
+		case "Alvik T-bana (Stockholm kn)":
+			fmt.Printf("%s \tSpånga station \t\t%s \t%.0fm \n", arrival.TransportNumber, arrival.Time, math.Ceil(delta.Minutes()))
+		case "Solna centrum T-bana":
+			fmt.Printf("%s \tBlackebergs gård \t%s \t%.0fm \n", arrival.TransportNumber, arrival.Time, math.Ceil(delta.Minutes()))
+		case "Tritonvägen (Sundbyberg kn)":
+			fmt.Printf("%s \tBlackebergs gård \t%s \t%.0fm \n", arrival.TransportNumber, arrival.Time, math.Ceil(delta.Minutes()))
+		}
+	}
+}
+
+type slAPIResponse struct {
 	Arrivals []arrival `json:"Arrival"`
 }
 
@@ -68,83 +166,7 @@ type stop struct {
 	DepDate  string `json:"depDate"`
 }
 
-func main() {
-
-	flag.BoolVar(&debug, "d", false, "print debug messages")
-	flag.Parse()
-
-	apikeyStopSign = os.Getenv("RESROBOTAPIKEY")
-	if apikeyStopSign == "" {
-		log.Fatal("no apikey present, use env var RESROBOTAPIKEY to set it")
-	}
-
-	apiRespBody, err := readStopSignAPI()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var response apiresponse
-	if err := json.Unmarshal(*apiRespBody, &response); err != nil {
-		log.Fatal(err)
-	}
-
-	// Sort response in two different directions
-
-	var resultInToCity []arrival
-	var resultOutOfCity []arrival
-
-	for _, arrival := range response.Arrivals {
-		switch arrival.Origin {
-		case "Spånga station (Stockholm kn)":
-			resultInToCity = append(resultInToCity, arrival)
-		case "Blackebergs gård (Stockholm kn)":
-			resultInToCity = append(resultInToCity, arrival)
-		case "Alvik T-bana (Stockholm kn)":
-			resultOutOfCity = append(resultOutOfCity, arrival)
-		case "Solna centrum T-bana":
-			resultOutOfCity = append(resultOutOfCity, arrival)
-		}
-	}
-
-	// Print result
-
-	fmt.Println("Bussar in mot stan: \n ")
-	fmt.Println("Linje:\tDestination:\tAnkomst:\tOm:")
-
-	for _, arrival := range resultInToCity {
-		deptime, err := time.Parse("2006-01-02T15:04:05", fmt.Sprintf("%sT%s", arrival.Date, arrival.Time))
-		if err != nil {
-			log.Fatal(err)
-		}
-		delta := deptime.Sub(time.Now().Add(time.Hour * 2)).Truncate(time.Minute)
-
-		switch arrival.Origin {
-		case "Spånga station (Stockholm kn)":
-			fmt.Printf("%s \tAlvik \t\t%s\t%.0fm \n", arrival.TransportNumber, arrival.Time, math.Ceil(delta.Minutes()))
-		case "Blackebergs gård (Stockholm kn)":
-
-			fmt.Printf("%s \tSolna centrum \t%s \t%.0fm \n", arrival.TransportNumber, arrival.Time, math.Ceil(delta.Minutes()))
-		}
-	}
-
-	fmt.Println("\n \nBussar ut från stan: \n ")
-	fmt.Println("Linje:\tDestination:\t\tAnkomst:\tOm:")
-	for _, arrival := range resultOutOfCity {
-		deptime, err := time.Parse("2006-01-02T15:04:05", fmt.Sprintf("%sT%s", arrival.Date, arrival.Time))
-		if err != nil {
-			log.Fatal(err)
-		}
-		delta := deptime.Sub(time.Now().Add(time.Hour * 2)).Truncate(time.Minute)
-
-		switch arrival.Origin {
-		case "Alvik T-bana (Stockholm kn)":
-			fmt.Printf("%s \tSpånga station \t\t%s \t%.0fm \n", arrival.TransportNumber, arrival.Time, math.Ceil(delta.Minutes()))
-		case "Solna centrum T-bana":
-			fmt.Printf("%s \tBlackebergs gård \t%s \t%.0fm \n", arrival.TransportNumber, arrival.Time, math.Ceil(delta.Minutes()))
-		}
-	}
-}
-
+// Read resrobots hållplats-api
 func readStopSignAPI() (*[]byte, error) {
 
 	client := http.Client{
@@ -173,5 +195,40 @@ func readStopSignAPI() (*[]byte, error) {
 		return nil, err
 	}
 
+	if debug {
+		if err := ioutil.WriteFile("result.json", body, 0777); err != nil {
+			return nil, err
+		}
+	}
+
 	return &body, nil
+}
+
+type apiResponse struct {
+	ArrivalsInToCity  []arrival `json:"arrivalsInToCity"`
+	ArrivalsOutOfCity []arrival `json:"arrivalsOutOfCity"`
+}
+
+// Sort response from resrobot api in two different directions based on origin
+func sortAPIResponse(response slAPIResponse) (result apiResponse) {
+
+	for _, arrival := range response.Arrivals {
+		if debug {
+			log.Println(arrival.Product, arrival.Origin)
+		}
+		switch arrival.Origin {
+		case "Spånga station (Stockholm kn)":
+			result.ArrivalsInToCity = append(result.ArrivalsInToCity, arrival)
+		case "Blackebergs gård (Stockholm kn)":
+			result.ArrivalsInToCity = append(result.ArrivalsInToCity, arrival)
+		case "Alvik T-bana (Stockholm kn)":
+			result.ArrivalsOutOfCity = append(result.ArrivalsOutOfCity, arrival)
+		case "Solna centrum T-bana":
+			result.ArrivalsOutOfCity = append(result.ArrivalsOutOfCity, arrival)
+		case "Tritonvägen (Sundbyberg kn)":
+			result.ArrivalsOutOfCity = append(result.ArrivalsOutOfCity, arrival)
+		}
+	}
+
+	return
 }
